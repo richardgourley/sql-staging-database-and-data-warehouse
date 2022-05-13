cffb927abc40d7a243b30c55f50b8fd9c34cd065
richardgourley <39519164+richardgourley@users.noreply.github.com>
2022-05-13 14:36:01 +0200

    Updated table examples added.

--- data_warehouse_tables.sql
+++ data_warehouse_tables.sql
@@ -1,85 +1,103 @@
-CREATE TABLE DimCloudProvider(
-provider_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-provider_key INT NOT NULL,
-provider_name VARCHAR(100) NOT NULL
-);
+BEGIN TRANSACTION
 
-CREATE TABLE DimCloudService(
-service_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-service_key VARCHAR(10) NOT NULL,
-service_name VARCHAR(100) NOT NULL,
-provider_name VARCHAR(100) NOT NULL,
-description TEXT NULL
-);
+BEGIN TRY
 
-CREATE TABLE DimOffice(
-office_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-office_key VARCHAR(10) NOT NULL,
-office_name VARCHAR(100) NOT NULL,
-office_street_address VARCHAR(100) NOT NULL,
-office_city VARCHAR(100) NOT NULL,
-office_state VARCHAR(100) NOT NULL,
-office_region VARCHAR(100) NOT NULL,
-office_country VARCHAR(100) NOT NULL
-);
+    CREATE TABLE DimCloudProvider(
+        provider_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        provider_key INT NOT NULL,
+        provider_name VARCHAR(100) NOT NULL
+    );
 
-CREATE TABLE DimRegion(
-region_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-region_name VARCHAR(100) NOT NULL
-);
+    CREATE TABLE DimCloudService(
+        service_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        service_key VARCHAR(10) NOT NULL,
+        service_name VARCHAR(100) NOT NULL,
+        description TEXT NULL
+    );
 
-CREATE TABLE DimCountry(
-country_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-country_name VARCHAR(100) NOT NULL
-);
+    CREATE TABLE DimOffice(
+        office_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        office_key VARCHAR(10) NOT NULL,
+        office_name VARCHAR(100) NOT NULL,
+        office_street_address VARCHAR(100) NOT NULL,
+        office_city VARCHAR(100) NOT NULL,
+        office_state VARCHAR(100) NOT NULL,
+        office_region VARCHAR(100) NOT NULL,
+        office_country VARCHAR(100) NOT NULL
+    );
 
-CREATE TABLE DimEmployee(
-employee_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-employee_key VARCHAR(10) NOT NULL,
-employee_name VARCHAR(100) NOT NULL
-);
+    CREATE TABLE DimRegion(
+        region_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        region_name VARCHAR(100) NOT NULL
+    );
 
-CREATE TABLE DimDate(
-date DATE NOT NULL,
-year SMALLINT NOT NULL,
-month_number CHAR(2) NOT NULL,
-month_name VARCHAR(10) NOT NULL,
-day_of_month CHAR(2) NOT NULL,
-quarter CHAR(2) NOT NULL,
-weekday_number TINYINT NOT NULL,
-weekday_name VARCHAR(10) NOT NULL,
-week_number TINYINT NOT NULL,
-year_month VARCHAR(20) NOT NULL,
-year_quarter CHAR(7) NOT NULL
-);
+    CREATE TABLE DimCountry(
+        country_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        country_name VARCHAR(100) NOT NULL
+    );
 
-CREATE TABLE FactIncidents(
-incident_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
-incident_key VARCHAR(10) NOT NULL,
-incident_date DATETIME NOT NULL,
-provider_surrogate_key INT NOT NULL,
-service_surrogate_key INT NOT NULL,
-office_surrogate_key INT NOT NULL,
-region_surrogate_key INT NOT NULL,
-country_surrogate_key INT NOT NULL,
-employee_surrogate_key INT NOT NULL,
-downtime_minutes NULL,
-    
-CONSTRAINT fk_provider_surrogate_key
-FOREIGN KEY (provider_surrogate_key) REFERENCES DimCloudProvider(provider_surrogate_key),
-    
-CONSTRAINT fk_service_surrogate_key
-FOREIGN KEY (service_surrogate_key) REFERENCES DimCloudService(service_surrogate_key),
-    
-CONSTRAINT fk_office_surrogate_key
-FOREIGN KEY (office_surrogate_key) REFERENCES DimOffice(office_surrogate_key),
-    
-CONSTRAINT fk_region_surrogate_key
-FOREIGN KEY (region_surrogate_key) REFERENCES DimRegion(region_surrogate_key),
-    
-CONSTRAINT fk_country_surrogate_key
-FOREIGN KEY (country_surrogate_key) REFERENCES DimCountry(country_surrogate_key),
-    
-CONSTRAINT fk_employee_surrogate_key
-FOREIGN KEY (employee_surrogate_key) REFERENCES DimEmployee(employee_surrogate_key)
-);
+    CREATE TABLE DimEmployee(
+        employee_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        employee_key VARCHAR(10) NOT NULL,
+        employee_name VARCHAR(100) NOT NULL
+    );
+
+    CREATE TABLE DimDate(
+        date DATE PRIMARY KEY NOT NULL,
+        year SMALLINT NOT NULL,
+        month_number CHAR(2) NOT NULL,
+        month_name VARCHAR(10) NOT NULL,
+        day_of_month CHAR(2) NOT NULL,
+        quarter CHAR(2) NOT NULL,
+        weekday_number TINYINT NOT NULL,
+        weekday_name VARCHAR(10) NOT NULL,
+        week_number TINYINT NOT NULL,
+        year_month VARCHAR(20) NOT NULL,
+        year_quarter CHAR(7) NOT NULL
+    );
+
+    CREATE TABLE FactIncidents(
+        incident_surrogate_key INT PRIMARY KEY IDENTITY(100,1),
+        incident_key VARCHAR(10) NOT NULL,
+        incident_date DATE NOT NULL,
+        provider_surrogate_key INT NOT NULL,
+        service_surrogate_key INT NOT NULL,
+        office_surrogate_key INT NOT NULL,
+        region_surrogate_key INT NOT NULL,
+        country_surrogate_key INT NOT NULL,
+        employee_surrogate_key INT NOT NULL,
+        downtime_minutes SMALLINT NULL,
+
+        CONSTRAINT fk_incident_date
+        FOREIGN KEY (incident_date) REFERENCES DimDate(date),
+
+        CONSTRAINT fk_provider_surrogate_key
+        FOREIGN KEY (provider_surrogate_key) REFERENCES DimCloudProvider(provider_surrogate_key),
+
+        CONSTRAINT fk_service_surrogate_key
+        FOREIGN KEY (service_surrogate_key) REFERENCES DimCloudService(service_surrogate_key),
+
+        CONSTRAINT fk_office_surrogate_key
+        FOREIGN KEY (office_surrogate_key) REFERENCES DimOffice(office_surrogate_key),
+
+        CONSTRAINT fk_region_surrogate_key
+        FOREIGN KEY (region_surrogate_key) REFERENCES DimRegion(region_surrogate_key),
+
+        CONSTRAINT fk_country_surrogate_key
+        FOREIGN KEY (country_surrogate_key) REFERENCES DimCountry(country_surrogate_key),
+
+        CONSTRAINT fk_employee_surrogate_key
+        FOREIGN KEY (employee_surrogate_key) REFERENCES DimEmployee(employee_surrogate_key)
+    );
+
+END TRY
+
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+    ROLLBACK TRANSACTION;
+
+END CATCH
+
+IF @@TRANCOUNT > 0
+    COMMIT TRANSACTION;
--- staging_database_tables.sql
+++ staging_database_tables.sql
@@ -1,74 +1,91 @@
-CREATE SCHEMA staging;
+CREATE SCHEMA staging
+GO
 
-CREATE TABLE staging.Offices(
-office_id INT PRIMARY KEY IDENTITY,
-office_key VARCHAR(10)
-office_name VARCHAR(100) NOT NULL,
-office_address VARCHAR(200) NOT NULL,
-office_city VARCHAR(100) NOT NULL,
-office_state VARCHAR(100) NOT NULL,
-office_region VARCHAR(100) NOT NULL,
-office_country VARCHAR(100) NOT NULL,
-date_opened DATETIME NOT NULL,
-date_closed DATETIME NULL,
-directions TEXT NULL
-);
+BEGIN TRANSACTION
 
-CREATE TABLE staging.Employees(
-employee_id INT PRIMARY KEY IDENTITY,
-employee_number VARCHAR(10)
-office_id INT NOT NULL,
-employee_first_name VARCHAR(100) NOT NULL,
-employee_last_name VARCHAR(100) NOT NULL,
-social_number VARCHAR(20) NOT NULL,
-date_of_birth DATETIME NOT NULL,
-start_date DATETIME NOT NULL,
-finish_date DATETIME NULL,
+BEGIN TRY
 
-CONSTRAINT fk_office_id
-FOREIGN KEY(office_id) REFERENCES staging.Offices(office_id)
-);
+    CREATE TABLE staging.Offices(
+        office_id INT PRIMARY KEY IDENTITY,
+        office_key VARCHAR(10),
+        office_name VARCHAR(100) NOT NULL,
+        office_address VARCHAR(200) NOT NULL,
+        office_city VARCHAR(100) NOT NULL,
+        office_state VARCHAR(100) NOT NULL,
+        office_region VARCHAR(100) NOT NULL,
+        office_country VARCHAR(100) NOT NULL,
+        date_opened DATETIME NOT NULL,
+        date_closed DATETIME NULL,
+        directions TEXT NULL
+    );
 
-CREATE TABLE staging.CloudProvider(
-provider_id INT PRIMARY KEY IDENTITY,
-provider_key VARCHAR(10) NOT NULL,
-provider_name VARCHAR(100) NOT NULL,
-provider_url VARCHAR(100) NOT NULL,
-provider_docs_url VARCHAR(100) NULL
-);
+    CREATE TABLE staging.Employees(
+        employee_id INT PRIMARY KEY IDENTITY,
+        employee_number VARCHAR(10),
+        employee_office_id INT NOT NULL,
+        employee_first_name VARCHAR(100) NOT NULL,
+        employee_last_name VARCHAR(100) NOT NULL,
+        social_number VARCHAR(20) NOT NULL,
+        date_of_birth DATETIME NOT NULL,
+        start_date DATETIME NOT NULL,
+        finish_date DATETIME NULL,
 
-CREATE TABLE staging.CloudService(
-service_id INT PRIMARY KEY IDENTITY,
-service_key VARCHAR(10) NOT NULL,
-service_name VARCHAR(100) NOT NULL,
-provider_id INT NOT NULL,
-    
-CONSTRAINT fk_provider_id
-FOREIGN KEY(provider_id) REFERENCES staging.CloudProvider(provider_id)
-);
+        CONSTRAINT fk_employee_office_id
+        FOREIGN KEY(employee_office_id) REFERENCES staging.Offices(office_id)
+    );
 
-CREATE TABLE staging.AcquiredCloudServices(
-acquired_service_id INT PRIMARY KEY IDENTITY,
-service_id INT NOT NULL,
-office_id INT NOT NULL,
-date_activated DATETIME NOT NULL,
-date_terminated DATETIME NULL,
-    
-CONSTRAINT fk_service_id
-FOREIGN KEY(service_id) REFERENCES staging.CloudService(service_id)
-    
-CONSTRAINT fk_office_id
-FOREIGN KEY(office_id) REFERENCES staging.Office(office_id)
-);
+    CREATE TABLE staging.CloudProvider(
+        provider_id INT PRIMARY KEY IDENTITY,
+        provider_key VARCHAR(10) NOT NULL,
+        provider_name VARCHAR(100) NOT NULL,
+        provider_url VARCHAR(100) NOT NULL,
+        provider_docs_url VARCHAR(100) NULL
+    );
 
-CREATE TABLE staging.Incidents(
-incident_id INT PRIMARY KEY IDENTITY,
-incident_date DATETIME NOT NULL,
-acquired_service_id INT NOT NULL,
-    -- Can be null if the incident has not been resolved
-downtime_minutes SMALLINT NULL,
-    
-CONSTRAINT fk_acquired_service_id
-FOREIGN KEY(acquired_service_id) REFERENCES AcquiredCloudServices(acquired_service_id)
-);
+    CREATE TABLE staging.CloudService(
+        service_id INT PRIMARY KEY IDENTITY,
+        service_key VARCHAR(10) NOT NULL,
+        service_name VARCHAR(100) NOT NULL,
+        provider_id INT NOT NULL,
+
+        CONSTRAINT fk_provider_id
+        FOREIGN KEY(provider_id) REFERENCES staging.CloudProvider(provider_id)
+    );
+
+    CREATE TABLE staging.AcquiredCloudServices(
+        acquired_service_id INT PRIMARY KEY IDENTITY,
+        service_id INT NOT NULL,
+        office_id INT NOT NULL,
+        date_activated DATETIME NOT NULL,
+        date_terminated DATETIME NULL,
+
+        CONSTRAINT fk_service_id
+        FOREIGN KEY(service_id) REFERENCES staging.CloudService(service_id),
+
+        CONSTRAINT fk_office_id
+        FOREIGN KEY(office_id) REFERENCES staging.Offices(office_id)
+    );
+
+    CREATE TABLE staging.Incidents(
+        incident_id INT PRIMARY KEY IDENTITY,
+        incident_date DATETIME NOT NULL,
+        acquired_service_id INT NOT NULL,
+            -- Can be null if the incident has not been resolved
+        downtime_minutes SMALLINT NULL,
+
+        CONSTRAINT fk_acquired_service_id
+        FOREIGN KEY(acquired_service_id) REFERENCES staging.AcquiredCloudServices(acquired_service_id)
+    );
+
+END TRY
+
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+    ROLLBACK TRANSACTION;
+
+END CATCH
+
+IF @@TRANCOUNT > 0
+    COMMIT TRANSACTION;
 
